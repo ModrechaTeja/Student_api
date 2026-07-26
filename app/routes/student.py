b1d9ad57c292a819c8e553ac4059aa2c1cc326e7
Modrecha Teja <148366161+ModrechaTeja@users.noreply.github.com>
2026-07-26 14:57:37 +0530

    resturctured project files

--- app/routes/student.py
+++ app/routes/student.py
@@ -1,21 +1,19 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-app=FastAPI()
+from fastapi import APIRouter, HTTPException
+from app.models.student import Student
+
+router = APIRouter()
 
 students = {}
 next_student_id = 1
 
-@app.get("/")
+
+@router.get("/")
 def home():
-    return {"message": "Welcome to stuudent api"}
+    return {"message": "Welcome to Student API"}
 
-class Student(BaseModel):
-    name:str
-    age:int
 
-@app.post("/student")
+@router.post("/student")
 def create_student(student: Student):
-
     global next_student_id
 
     students[next_student_id] = {
@@ -31,13 +29,14 @@ def create_student(student: Student):
         "student": students[next_student_id - 1]
     }
 
-@app.get("/students")
+
+@router.get("/students")
 def get_students():
     return students
 
-@app.get("/student/{student_id}")
-def get_student(student_id: int):
 
+@router.get("/student/{student_id}")
+def get_student(student_id: int):
     if student_id not in students:
         raise HTTPException(
             status_code=404,
@@ -46,7 +45,8 @@ def get_student(student_id: int):
 
     return students[student_id]
 
-@app.put("/student/{student_id}")
+
+@router.put("/student/{student_id}")
 def update_student(student_id: int, student: Student):
 
     if student_id not in students:
@@ -66,7 +66,8 @@ def update_student(student_id: int, student: Student):
         "student": students[student_id]
     }
 
-@app.delete("/student/{student_id}")
+
+@router.delete("/student/{student_id}")
 def delete_student(student_id: int):
 
     if student_id not in students:
